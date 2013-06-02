c48969eaddbb88dfa5b8224abbe4c873cb35d4af
Tim S <tim@blendinteractive.com>
2013-06-02 01:00:46 -0500

    updated footer, main content and main nav drop down styles

--- themes/harvestsf2013/footer.php
+++ themes/harvestsf2013/footer.php
@@ -18,91 +18,97 @@
 							<div id="map" style="height: 215px;"></div>
 				    	</address>
 			    		<script type="text/javascript">
-			    			var styles = [
-								{
-									featureType: 'landscape',
-									elementType: 'all',
-									stylers: [
-										{ hue: '#2d2e2e' },
-										{ saturation: -96 },
-										{ lightness: -80 },
-										{ visibility: 'on' }
-									]
-								},{
-									featureType: 'poi',
-									elementType: 'all',
-									stylers: [
-										{ hue: '#2d2e2e' },
-										{ saturation: -97 },
-										{ lightness: -77 },
-										{ visibility: 'off' }
-									]
-								},{
-									featureType: 'road.local',
-									elementType: 'all',
-									stylers: [
-										{ hue: '#444646' },
-										{ saturation: -99 },
-										{ lightness: -73 },
-										{ visibility: 'simplified' }
-									]
-								},{
-									featureType: 'road.arterial',
-									elementType: 'all',
-									stylers: [
-										{ hue: '#737676' },
-										{ saturation: -99 },
-										{ lightness: -41 },
-										{ visibility: 'on' }
-									]
-								},{
-									featureType: 'administrative',
-									elementType: 'all',
-									stylers: [
-										{ hue: '#000000' },
-										{ saturation: 0 },
-										{ lightness: -100 },
-										{ visibility: 'off' }
-									]
-								},{
-									featureType: 'road.highway',
-									elementType: 'all',
-									stylers: [
-										{ hue: '#959696' },
-										{ saturation: -100 },
-										{ lightness: -8 },
-										{ visibility: 'on' }
-									]
-								},{
-									featureType: 'water',
-									elementType: 'all',
-									stylers: [
-										{ hue: '#004a74' },
-										{ saturation: 100 },
-										{ lightness: -70 },
-										{ visibility: 'on' }
-									]
-								}
-							];
-							var harvestLatLang = new google.maps.LatLng(43.5455, -96.683);
-							var options = {
-								mapTypeControlOptions: {
-									mapTypeIds: [ 'Styled']
-								},
-								center: harvestLatLang,
-								zoom: 14,
-								mapTypeId: 'Styled'
-							};
-							var div = document.getElementById('map');
-							var map = new google.maps.Map(div, options);
-							var icon = '<?php echo get_template_directory_uri(); ?>/assets/img/google-map-icon.png';
-							var marker = new google.maps.Marker({position: harvestLatLang, map: map, icon: icon});
-							var styledMapType = new google.maps.StyledMapType(styles, { name: 'Styled' });
-							map.mapTypes.set('Styled', styledMapType);
-			    		</script>
+				    			var styles = [
+									{
+										featureType: 'landscape',
+										elementType: 'all',
+										stylers: [
+											{ hue: '#2d2e2e' },
+											{ saturation: -96 },
+											{ lightness: -80 },
+											{ visibility: 'on' }
+										]
+									},{
+										featureType: 'poi',
+										elementType: 'all',
+										stylers: [
+											{ hue: '#2d2e2e' },
+											{ saturation: -97 },
+											{ lightness: -77 },
+											{ visibility: 'off' }
+										]
+									},{
+										featureType: 'road.local',
+										elementType: 'all',
+										stylers: [
+											{ hue: '#444646' },
+											{ saturation: -99 },
+											{ lightness: -73 },
+											{ visibility: 'simplified' }
+										]
+									},{
+										featureType: 'road.arterial',
+										elementType: 'all',
+										stylers: [
+											{ hue: '#737676' },
+											{ saturation: -99 },
+											{ lightness: -41 },
+											{ visibility: 'on' }
+										]
+									},{
+										featureType: 'administrative',
+										elementType: 'all',
+										stylers: [
+											{ hue: '#000000' },
+											{ saturation: 0 },
+											{ lightness: -100 },
+											{ visibility: 'off' }
+										]
+									},{
+										featureType: 'road.highway',
+										elementType: 'all',
+										stylers: [
+											{ hue: '#959696' },
+											{ saturation: -100 },
+											{ lightness: -8 },
+											{ visibility: 'on' }
+										]
+									},{
+										featureType: 'water',
+										elementType: 'all',
+										stylers: [
+											{ hue: '#004a74' },
+											{ saturation: 100 },
+											{ lightness: -70 },
+											{ visibility: 'on' }
+										]
+									}
+								];
+								var harvestLatLang = new google.maps.LatLng(43.5455, -96.683);
+								var options = {
+									mapTypeControlOptions: {
+										mapTypeIds: [ 'Styled']
+									},
+									center: harvestLatLang,
+									zoom: 14,
+									mapTypeId: 'Styled'
+								};
+								var div = document.getElementById('map');
+								var map = new google.maps.Map(div, options);
+								var icon = '<?php echo get_template_directory_uri(); ?>/assets/img/google-map-icon.png';
+								var marker = new google.maps.Marker({position: harvestLatLang, map: map, icon: icon});
+								var styledMapType = new google.maps.StyledMapType(styles, { name: 'Styled' });
+								map.mapTypes.set('Styled', styledMapType);
+			    		</script>	
 				    </div>
 				    <div class="span4">
-					    <img src="<?php echo get_template_directory_uri(); ?>/assets/img/follow-us-placeholder.png" alt="" />
+					    <h2>
+					    	<div class="text">Follow Us</div>
+						    <a href="http://facebook.com/harvestsf" class="fb" title="facebook"><span>facebook</span></a>
+						    <a href="http://twitter.com/harvestsf" class="tw" title="twitter"><span>twitter</span></a>
+						    <div class="clearboth"></div>
+					    </h2>
+					    <a href="http://instagram.com/harvestsf" titl="http://instagram.com/harvestsf" target="_blank" class="insta"><img src="<?php echo get_template_directory_uri(); ?>/assets/img/footer-insta-default.jpg" alt="http://instagram.com/harvestsf" /></a>
 					</div>
 			    </footer>
 			</div><!-- .container -->
