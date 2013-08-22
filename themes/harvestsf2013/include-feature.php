6ddd8a8266f3cb9344d91a3921fa48e2ada92044
Tim S <tim@blendinteractive.com>
2013-08-22 08:07:12 -0500

    updates to mobile nav & 404 page

--- themes/harvestsf2013/include-feature.php
+++ themes/harvestsf2013/include-feature.php
@@ -1,33 +1,37 @@
 <?php
 /**
  * Include file for the feature header section on interior pages
- * 
+ *
  */
 ?>
 
-	<div class="feature">
-		<div class="breadcrumb">
-		<?php 
-			if (function_exists('bcn_display')) {
-		        bcn_display();
-		    } 
-		?>
-		</div>
-		
-        <?php if ( is_single() ) : ?>
-        
-	        <h1 class="entry-title"><?php the_title(); ?></h1>
+<div class="feature">
+    <div class="breadcrumb">
+        <?php
+        if (function_exists('bcn_display')) {
+            bcn_display();
+        }
+        ?>
+    </div>
 
-        <?php elseif ( is_category() ) : ?>
+    <?php if ( is_single() ) : ?>
 
-            <h1 class="entry-title"><?php single_cat_title(); ?></h1>
+        <h1 class="entry-title"><?php the_title(); ?></h1>
 
-        <?php else : ?>
-        
-	        <h1 class="entry-title">
-	            <a href="<?php the_permalink(); ?>" title="<?php echo esc_attr( sprintf( __( 'Permalink to %s', 'harvestsf' ), the_title_attribute( 'echo=0' ) ) ); ?>" rel="bookmark"><?php the_title(); ?></a>
-	        </h1>
-        
-        <?php endif; ?>
-        
-	</div>
+    <?php elseif ( is_category() ) : ?>
+
+        <h1 class="entry-title"><?php single_cat_title(); ?></h1>
+
+    <?php elseif ( is_404() ) : ?>
+
+        <h1 class="entry-title">Oops, Page Not Found</h1>
+
+    <?php else : ?>
+
+        <h1 class="entry-title">
+            <a href="<?php the_permalink(); ?>" title="<?php echo esc_attr( sprintf( __( 'Permalink to %s', 'harvestsf' ), the_title_attribute( 'echo=0' ) ) ); ?>" rel="bookmark"><?php the_title(); ?></a>
+        </h1>
+
+    <?php endif; ?>
+
+</div>
